3f15e79a03e0d5df0b0a7c73fbc26e85d376c6ce
TruxSux <kaithomasdean@outlook.com>
2022-04-12 23:06:31 -1000

    updated prettier to support CRLF endOfLine

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './styles.module.css'
-export { default as Btn } from "./btns/Btn"
-export { default as Input } from "./forms/Input"
+export { default as Btn } from './btns/Btn'
+export { default as Input } from './forms/Input'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.box}>
+  return (
+    <div className={styles.box}>
       <p className={styles.text}>{text}</p>
     </div>
+  )
 }
