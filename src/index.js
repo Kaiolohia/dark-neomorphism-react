552eac0bfcb8127d8bb9861b4e48251d9c877e8e
TruxSux <kaithomasdean@outlook.com>
2022-04-14 21:39:59 -1000

    Message to self

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import styles from './styles.module.css'
 export { default as Btn } from './btns/Btn'
 export { default as Input } from './forms/Input'
 
+// https://github.com/mui/material-ui/blob/master/packages/mui-material/src/Card/Card.js
+// should follow MUI code design for handling components
 export const Card = ({ title, children }) => {
   return (
     <div className={styles.box}>
